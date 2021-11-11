aa97df35220f4e717d7cd8bac68fd008592147e3
Sergei Konstantinov <konstantinov-sa@bk.ru>
2021-11-11 17:51:11 +0300

    new v1.0.0

--- myModule.go
+++ myModule.go
@@ -6,7 +6,11 @@ func Demo(x int) int {
 	return x * x
 }
 
+func Demo2(x int) int {
+	return x * x * x
+}
+
 func Version() {
 	fmt.Println("actual str")
-	fmt.Println("Version 1.0.2")
+	fmt.Println("Version 1.1.0")
 }
